60d150f68c3729f9cef2733989e6662ac8ae09d8
franklin-raph <igboekwulusifranklin@gmail.com>
2023-04-20 15:30:10 +0100

    updated the course benefits and contents

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// typed effect
+let typed = new Typed(".typing-effect", {
+  strings: ["FRONT-END DEVELOPER.", "BACK-END DEVELOPER.", "FULL-STACK DEVELOPER."],
+  backSpeed: "9",
+  typeSpeed: "9",
+  loop: true,
+});
