9ad80bf7f96867d76334bd465bae621c8861ead7
franklin-raph <igboekwulusifranklin@gmail.com>
2023-04-24 19:01:57 +0100

    update

--- js/main.js
+++ js/main.js
@@ -5,3 +5,48 @@ let typed = new Typed(".typing-effect", {
   typeSpeed: "9",
   loop: true,
 });
+
+
+$('.sliders').slick({
+  dots: true,
+  infinite: true,
+  speed: 400,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        autoplay: true,
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        autoplay: true,
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        autoplay: true,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
